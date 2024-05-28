dcccdc799825aea2f793bf223dd0c61f572a9e3e
Jong-Kon, Youk <excelon@live.co.kr>
2024-05-29 08:12:15 +0900

    Update util.py
    .

--- common/util.py
+++ common/util.py
@@ -9,3 +9,14 @@ def file_save(file_name):
     except Exception as e:
         logger.error(e)
         return "fail"
+
+def return_imagr_stream(img_local_path):
+   img_stream =''
+   with open(img_local_path,'rb') as im_f:
+       img_stream =img_f.read()
+       img_stream =base64.b64encode(img_stream).decode()
+    return img_stream
+
+def nvl(a,b):
+    if a== None or a == '': return b
+    else: return a
