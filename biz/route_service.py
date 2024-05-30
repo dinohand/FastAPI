c29c5118de77f64d75de62517d18d876b57f9f15
JK.Youk <[excelon@live.co.kr]>
2024-05-30 23:57:01 +0900

    .

--- biz/route_service.py
+++ biz/route_service.py
@@ -1,14 +1,42 @@
 from common.log_manager import Log_Manager
+
+from fastapi import FastAPI
+from fastapi import Request, Response, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+# 라우트에 따른 처리 프로세스
 class Route_Service:
     logger = None
     def __init__(self) -> None:
         self.logger = Log_Manager().getLogger( type(self).__name__ )
+        self.templates = Jinja2Templates(directory="static")   ### templates.TemplateResponse를 사용하려면 Route_Service에서 선언되어야 있어야 한다
 
-    async def heathCheck(self):
+    ## root page route
+    def root(self) -> str:
+        status_code = 200
+        message =  "This is root page !"
+        return {"message": message, "code": status_code}
+
+    async def heathCheck(self) -> str:
         # self.logger.info("health check ---------------")
         return {"message":"ok", "code":200}
     
     def hello(self):
         # self.logger.info("hello----------------")
         return "You need REST Api call"
-    
+
+    # Static html을 서비스하는 route
+    async def item(self, request : Request,  item : str, response_class=HTMLResponse ):
+        return self.templates.TemplateResponse(
+            request=request, name="test.html", context={"item_name": item}
+        )
+
+    # async def read_item(request: Request, item: str):
+#     return templates.TemplateResponse(
+#         request=request, name="test.html", context={"item_name": item}
+#     )   
+    def test(self):
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # from main import app 
 from fastapi import FastAPI
+from fastapi import Response, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 #  import uvicorn
 from common.config_manager import Config_Manager
 from common.log_manager import Log_Manager
@@ -11,22 +15,77 @@ logger = Log_Manager().getLogger("MAIN")
 cm = Config_Manager() ## config manager
 rs = Route_Service()
 
+# swagger_ui_default_parameters: Annotated[
+#     Dict[str, Any],
+#     Doc(
+#         # """
+#         # Default configurations for Swagger UI.
 
-app = FastAPI()
+#         # You can use it as a template to add any other configurations needed.
+#         # """
+#     ),
+# ] = {
+#     "dom_id": "#swagger-ui",
+#     "layout": "BaseLayout",
+#     "deepLinking": True,
+#     "showExtensions": True,
+#     "showCommonExtensions": True,
+# }
+
+
+app = FastAPI(
+            # swagger_ui_parameters=swagger_ui_default_parameters,
+            swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"},
+            title='title',
+            summary='summary',
+            description='Boilerplate',
+            version='0.1',
+            docs_url='/api/v1/docs',
+            redoc_url='/api/v1/redocs',
+            terms_of_service="", ## "http://example.com/terms/",
+            contact={
+                "name": "JK.Youk",
+                "url":  "https://github.com/dinohand",
+                "email": "excelon@live.co.kr",
+            },            
+            icense_info={
+                "name": "Apache 2.0",
+                "identifier": "MIT",
+                #  "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+            },
+        )
+
+# tags_metadata = [
+#     {
+#         "name": "users",
+#         "description": "Operations with users. The **login** logic is also here.",
+#     },
+#     {
+#         "name": "items",
+#         "description": "Manage items. So _fancy_ they have their own docs.",
+#         "externalDocs": {
+#             "description": "Items external docs",
+#             "url": "https://fastapi.tiangolo.com/",
+#         },
+#     },
+# ]
+# app = FastAPI(openapi_tags=tags_metadata)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
+templates = Jinja2Templates(directory="static")
 # app.include_router(static.router)
 
-logger.info("App is started !")
+#--------------------------
+# @app.get('/')
+# def root():
+#     return rs.root()
 
-
-@app.get('/')
-@app.post('/')
-def hello():
-    return 'Hello'
-
-@app.get('/health')
-async def healthcheck():
+@app.get('/',  response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={} 
+    )
+async def check_health():
     return await rs.heathCheck()
     
     # return {'message':'health check ok', 'code': 200}
@@ -42,5 +101,11 @@ async def upload():
     f = request.files['file']
     f.save(f.filename)
     return '성공 여부 try except로 처리 계획'
+
+@app.get('/item/{item}', response_class=HTMLResponse)
+async def name_of_item(request : Request, item ):
+    return await rs.item(request, item) 
+ 
+
 
 
